5f9e3bef10849cbfc592545206b6b1fc2e7c4b3c
Joana06083 <joana06083@gmail.com>
2022-05-06 17:47:04 +0800

    20220506-modify Check

--- app/Http/Controllers/GetApiController.php
+++ app/Http/Controllers/GetApiController.php
@@ -2,15 +2,11 @@
 
 namespace App\Http\Controllers;
 
-// use GuzzleHttp\Client;
-// use Illuminate\Http\Request;
 use App\Models\openData;
 use Illuminate\Support\Facades\Http;
 
 class GetApiController extends Controller
 {
-    //
-
     public function index()
     {
         $ActivityInfo = openData::paginate(10);
@@ -26,41 +22,56 @@ class GetApiController extends Controller
             'https://datacenter.taichung.gov.tw/swagger/OpenData/c6abbb99-b617-4379-8562-4ab115e91ead',
             [
                 'operationId' => 'c6abbb99-b617-4379-8562-4ab115e91ead',
-                // 'limit' => 10, //抓取幾筆
+                // 'limit' => 5, //抓取幾筆
             ]);
+
         $Activity = json_decode($response->body());
         $ActivityTime = '活動展演(起訖)';
         $arrData = [];
+
         foreach ($Activity as $act) {
-
-            // 檢驗是否重複再寫入資料庫
-            $ActCheck = openData::where([
-                ['ActivityName', $act->活動名稱],
-                ['ActivityTime', $act->$ActivityTime],
-                ['ActivityTicket', $act->活動售票與否],
-                ['ActivityPlace', $act->地點],
-                ['ActivityTicketPrice', $act->票價],
-                ['ActivityImage', $act->相關圖片],
-                ['ActivityUrl', $act->活動網址],
-                ['ActivityEnter', $act->入場方式],
-            ])->first();
-
-            if (!$ActCheck) {
-                array_push($arrData, [
-                    'ActivityName' => $act->活動名稱,
-                    'ActivityTime' => $act->$ActivityTime,
-                    'ActivityTicket' => $act->活動售票與否,
-                    'ActivityPlace' => $act->地點,
-                    'ActivityTicketPrice' => $act->票價,
-                    'ActivityImage' => $act->相關圖片,
-                    'ActivityUrl' => $act->活動網址,
-                    'ActivityEnter' => $act->入場方式,
-                ]
-                );
-            }
+            $data = [
+                'ActivityName' => $act->活動名稱,
+                'ActivityTime' => $act->$ActivityTime,
+                'ActivityTicket' => $act->活動售票與否,
+                'ActivityPlace' => $act->地點,
+                'ActivityTicketPrice' => $act->票價,
+                'ActivityImage' => $act->相關圖片,
+                'ActivityUrl' => $act->活動網址,
+                'ActivityEnter' => $act->入場方式,
+            ];
+            array_push($arrData, $data);
         }
-        openData::insert($arrData);
-        return redirect('/');
+
+        $ActCheck = openData::whereIn('ActivityName', array_column($arrData, 'ActivityName'))
+            ->whereIn('ActivityTime', array_column($arrData, 'ActivityTime'))
+            ->whereIn('ActivityTicket', array_column($arrData, 'ActivityTicket'))
+            ->whereIn('ActivityPlace', array_column($arrData, 'ActivityPlace'))
+            ->whereIn('ActivityTicketPrice', array_column($arrData, 'ActivityTicketPrice'))
+            ->whereIn('ActivityImage', array_column($arrData, 'ActivityImage'))
+            ->whereIn('ActivityUrl', array_column($arrData, 'ActivityUrl'))
+            ->whereIn('ActivityEnter', array_column($arrData, 'ActivityEnter'))
+            ->select('ActivityName', 'ActivityTime', 'ActivityTicket', 'ActivityPlace', 'ActivityTicketPrice', 'ActivityImage', 'ActivityUrl', 'ActivityEnter')
+            ->get();
+
+        // $ActCheck = openData::select('ActivityName', 'ActivityTime', 'ActivityTicket', 'ActivityPlace', 'ActivityTicketPrice', 'ActivityImage', 'ActivityUrl', 'ActivityEnter')
+        //     ->get();
+        // $ActCheck2 = openData::select('ActivityName', 'ActivityTime', 'ActivityTicket', 'ActivityPlace', 'ActivityTicketPrice', 'ActivityImage', 'ActivityUrl', 'ActivityEnter')
+        //     ->take(2)->get();
+        // $arr1 = json_decode($ActCheck, true);
+        // $arr2 = json_decode($ActCheck2, true);
+
+        if (!isset($ActCheck)) {
+            //資料庫不為空，但無重複資料
+            return redirect('/');
+        }
+
+        //只顯示不重複的資料
+        $ActCheckarr = json_decode($ActCheck, true);
+        $diff = array_diff(array_map('serialize', $arrData), array_map('serialize', $ActCheckarr));
+        $result = array_map('unserialize', $diff);
+
+        openData::insert($result);
 
     }
 
