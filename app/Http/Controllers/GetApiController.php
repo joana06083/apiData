cc1d73f19c84aafeddba85ad1cd8264ac4803b00
Joana06083 <joana06083@gmail.com>
2022-05-06 09:51:04 +0800

    20220506-modify insert

--- app/Http/Controllers/GetApiController.php
+++ app/Http/Controllers/GetApiController.php
@@ -10,6 +10,16 @@ use Illuminate\Support\Facades\Http;
 class GetApiController extends Controller
 {
     //
+
+    public function index()
+    {
+        $ActivityInfo = openData::paginate(10);
+        $data = [
+            'ActivityInfo' => $ActivityInfo,
+        ];
+
+        return view('index', $data);
+    }
     public function saveApiData()
     {
         $response = Http::get(
@@ -20,18 +30,8 @@ class GetApiController extends Controller
             ]);
         $Activity = json_decode($response->body());
         $ActivityTime = '活動展演(起訖)';
-
+        $arrData = [];
         foreach ($Activity as $act) {
-
-            $openData = new openData;
-            $openData->ActivityName = $act->活動名稱;
-            $openData->ActivityTime = $act->$ActivityTime;
-            $openData->ActivityTicket = $act->活動售票與否;
-            $openData->ActivityPlace = $act->地點;
-            $openData->ActivityTicketPrice = $act->票價;
-            $openData->ActivityImage = $act->相關圖片;
-            $openData->ActivityUrl = $act->活動網址;
-            $openData->ActivityEnter = $act->入場方式;
 
             // 檢驗是否重複再寫入資料庫
             $ActCheck = openData::where([
@@ -46,20 +46,22 @@ class GetApiController extends Controller
             ])->first();
 
             if (!$ActCheck) {
-                $openData->save();
+                array_push($arrData, [
+                    'ActivityName' => $act->活動名稱,
+                    'ActivityTime' => $act->$ActivityTime,
+                    'ActivityTicket' => $act->活動售票與否,
+                    'ActivityPlace' => $act->地點,
+                    'ActivityTicketPrice' => $act->票價,
+                    'ActivityImage' => $act->相關圖片,
+                    'ActivityUrl' => $act->活動網址,
+                    'ActivityEnter' => $act->入場方式,
+                ]
+                );
             }
         }
-
+        openData::insert($arrData);
         return redirect('/');
 
     }
-    public function index()
-    {
-        $ActivityInfo = openData::paginate(10);
-        $data = [
-            'ActivityInfo' => $ActivityInfo,
-        ];
 
-        return view('index', $data);
-    }
 }
